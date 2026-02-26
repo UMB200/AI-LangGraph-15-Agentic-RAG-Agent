0d26be4d34030f92dd9d2e22269e4475e17681ad
UMB200 <16376967+UMB200@users.noreply.github.com>
2026-02-26 16:11:10 -0500

    updated tests added multiple classes

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,3 +1,25 @@
-def test_full() -> None:
-    assert 1 == 1
-    
+from dotenv import load_dotenv
+load_dotenv()
+
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from ingestion import retriever
+
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+    result: GradeDocuments = retrieval_grader.invoke({
+        "question": question, 
+        "document": doc_txt
+    })
+    assert result.binary_score == "yes"
+
+def test_retrieval_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+    result: GradeDocuments = retrieval_grader.invoke({
+        "question": "how to bake a chocolate cake", 
+        "document": doc_txt
+    })
+    assert result.binary_score == "no"
